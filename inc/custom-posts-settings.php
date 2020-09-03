dddc1886eafc2fca2d758a569786b2239e5264ad
wazid <wazidshh@gmail.com>
2020-09-03 16:12:51 +0530

    fix: cpt and acf

--- inc/custom-posts-settings.php
+++ inc/custom-posts-settings.php
@@ -210,5 +210,51 @@ function cptui_register_my_taxes() {
 		"show_in_quick_edit" => false,
 			];
 	register_taxonomy( "organisation_category", [ "organisations" ], $args );
+
+	/**
+	 * Taxonomy: Availabilities.
+	 */
+
+	$labels = [
+		"name" => __( "Availabilities", "custom-post-type-ui" ),
+		"singular_name" => __( "Availability", "custom-post-type-ui" ),
+		"menu_name" => __( "Availabilities", "custom-post-type-ui" ),
+		"all_items" => __( "All Availabilities", "custom-post-type-ui" ),
+		"edit_item" => __( "Edit Availability", "custom-post-type-ui" ),
+		"view_item" => __( "View Availability", "custom-post-type-ui" ),
+		"update_item" => __( "Update Availability name", "custom-post-type-ui" ),
+		"add_new_item" => __( "Add new Availability", "custom-post-type-ui" ),
+		"new_item_name" => __( "New Availability name", "custom-post-type-ui" ),
+		"parent_item" => __( "Parent Availability", "custom-post-type-ui" ),
+		"parent_item_colon" => __( "Parent Availability:", "custom-post-type-ui" ),
+		"search_items" => __( "Search Availabilities", "custom-post-type-ui" ),
+		"popular_items" => __( "Popular Availabilities", "custom-post-type-ui" ),
+		"separate_items_with_commas" => __( "Separate Availabilities with commas", "custom-post-type-ui" ),
+		"add_or_remove_items" => __( "Add or remove Availabilities", "custom-post-type-ui" ),
+		"choose_from_most_used" => __( "Choose from the most used Availabilities", "custom-post-type-ui" ),
+		"not_found" => __( "No Availabilities found", "custom-post-type-ui" ),
+		"no_terms" => __( "No Availabilities", "custom-post-type-ui" ),
+		"items_list_navigation" => __( "Availabilities list navigation", "custom-post-type-ui" ),
+		"items_list" => __( "Availabilities list", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Availabilities", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'availability', 'with_front' => true, ],
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "availability",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+			];
+	register_taxonomy( "availability", [ "event_listing" ], $args );
 }
 add_action( 'init', 'cptui_register_my_taxes' );
